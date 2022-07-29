c6f3ac97d67e11354ae608793c9cf9685e95689c
Ausdauer <100087648+jmoreno1852@users.noreply.github.com>
2022-07-29 10:42:32 +0200

    Challenge 5

--- ch_5.py
+++ ch_5.py
@@ -4,3 +4,22 @@
 # getString: to get a string from console input
 # printString: to print the string in upper case.
 # Also please include simple test function to test the class methods.
+
+class TestClass():
+    def __init__(self):
+        pass
+
+    def getString(self):
+        self.string = str(input("Write something! "))
+
+    def printString(self):
+        print(self.string.upper())
+
+
+def test():
+    clase = TestClass()
+    clase.getString()
+    clase.printString()
+
+
+test()
