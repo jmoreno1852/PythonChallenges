aa9f48890cddc7dba4af4238313cbb7d23fb6c7b
Ausdauer <100087648+jmoreno1852@users.noreply.github.com>
2022-08-03 11:21:16 +0200

    Challenge 29

---
+++ ch_29.py
@@ -0,0 +1,8 @@
+# Question:
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+def ft_strconcat(str1, str2):
+    return print(f'{str1} {str2}')
+
+
+ft_strconcat("hola", "mundo")
