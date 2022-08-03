4919382f26d3d736f476d9618cf7a603e653fd59
Ausdauer <100087648+jmoreno1852@users.noreply.github.com>
2022-08-03 11:18:23 +0200

    Challenge 28

---
+++ ch_28.py
@@ -0,0 +1,10 @@
+# Question:
+# Define a function that can receive two integer numbers in string
+# form and compute their sum and then print it in console.
+
+def ft_strsum(x, y):
+    return int(x)+int(y)
+
+
+print(ft_strsum("1", "2"))
+print(type(ft_strsum("1", "2")))
